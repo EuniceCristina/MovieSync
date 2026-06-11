dcf8a2e1d6376286463043e1b0f84c8083e7e072
eunicecristina <eunicecristina0510@gmail.com>
2026-06-11 17:48:11 -0300

    commt

--- teste.py
+++ teste.py
@@ -1,11 +1,8 @@
-dados_pessoa = {
-    "nome": "Ana",
-    "idade": 28,
-    "cidade": "Natal"
-}
-# Adicionando um novo dado
-dados_pessoa["profissão"] = "Engenheira"
-
-# Modificando um dado existente
-dados_pessoa["idade"] = 29
-print(dados_pessoa.get("idade", "Não informada"))
+disc = 'Grogramação'
+print('Disciplina: ', disc)
+lista = list(disc)
+print(lista)
+lista[0] = 'P'
+print(lista)
+disc = ''.join(lista)
+print('Disciplina: ', disc)
